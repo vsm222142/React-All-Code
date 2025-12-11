bf3d9cf8d5f2d23922cb92b8eec98c009a5ff9c2
Vishal <mauryavishal0080@gmail.com>
2025-12-11 13:24:58 +0530

    updates

--- Day4/navigation/src/component/Home.jsx
+++ Day4/navigation/src/component/Home.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
+export default function Home() {
 
-export default function Home(){
+
+    const [isLogin, setIsLogout] = useState(false);  // hook-useState()
     return (
         <>
-        <div>
-            <h1>Home Page</h1>
-        </div>
+            <div>
+                <h1>Home Page</h1>
+
+                {isLogin && <p> You are Logged in</p>}
+                <button onClick={() => setIsLogout(!isLogin)}>
+                    {isLogin ? "Logout" : "Login"}
+                </button>
+            </div>
         </>
     )
 }
