462e66671cc6d4e3dccb99478fbb06de9d6212f7
Vishal <mauryavishal0080@gmail.com>
2025-12-19 12:56:02 +0530

    Updates

--- Day5/hooksproject/src/App.jsx
+++ Day5/hooksproject/src/App.jsx
@@ -8,7 +8,9 @@ import EffectDemo from './components/EffectDemo';
 import StopWatch from './components/StopWatch';
 import StateDemo2 from './components/StateDemo2';
 import { ContextDemo, ThemeButton } from './components/ContextDemo';
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import {Home,About} from "./components/Navigate";
+import Memo from './components/Memo';
 function App() {
 
 
@@ -21,13 +23,26 @@ function App() {
       {/* <ParamDeemo />
       <EffectDemo />
       <StopWatch/> */}
-      <StateDemo2 />
+      {/* <StateDemo2 />
 
      <ContextDemo>
       <ThemeButton/>
-     </ContextDemo>
+     </ContextDemo> */}
+
+     <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<Home/>}/>
+
+      <Route path="/about" element={<About/>}/>
+
+   
+    
+      </Routes>
+      </BrowserRouter>
+      <Memo/>
       
     </>
+    
   )
 }
 
